4f8c19b1648e388675fb6cde70d2f9a191573546
how0421 <s1120534@dmhs.kh.edu.tw>
2025-11-07 11:40:48 +0800

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"iamgod" << std::endl;
+    std::cout<<"i am god" << std::endl;
+    std::cout<<"i\tam\tgod" << std::endl;
+    std::cout<<"i\nam\ngod" << std::endl;
+    std::cout<<"\"iam god\"" << std::endl;
+    std::cout<<"i\\am\\god" << std::endl;
+
+    return 0;
+}
